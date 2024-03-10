5c7961fa3c87615ec3f5273067d60120069fb0a0
kathiravan-17-2005 <162541043+kathiravan-17-2005@users.noreply.github.com>
2024-03-10 12:12:05 +0530

    first page

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ========================== typing animation ========================= */
+var typed = new Typed(".typing",{
+    strings:[""," Junior Frontend developer"," Junior web Designer","Graphic Designer","Enthusiast"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
